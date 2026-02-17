6f5707bca28795520d033170263000000db4304b
ENOKI TOKI <tokisora123456@gmail.com>
2026-02-17 23:24:23 +0900

    テンプレートファイルのコピー Add PWA support with manifest and service worker implementation
    - Created a manifest.json file for the PWA with app details, icons, and shortcuts.
    - Implemented a service worker (sw.js) for caching assets and handling offline functionality.
    - Added push notification handling and notification click events in the service worker.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+import vuetify from 'vite-plugin-vuetify'
+require('dotenv').config()
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxtjs/i18n',
     '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
     'vuetify-nuxt-module',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  runtimeConfig: {
+    public: {
+      // @ts-ignore
+      env: process.env,
+    },
+  },
+  ssr: true,
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          property: 'og:image',
+          /** 相対パスNGらしいので各自で書き換えること */
+          content: 'https://nuxt4.enoki.xyz/img/thumbnail.jpg',
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+        { name: 'apple-mobile-web-app-title', content: 'APP_TITLE' },
+        { name: 'theme-color', content: '#000000' },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:creator', content: '@jikantoki' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/manifest.json' },
+      ],
+    },
+  },
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
